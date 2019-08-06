98dc6a4eae807419bd26264f8a7182ce03803631
Maker-Mark <markgoldstein12@gmail.com>
2019-08-06 15:31:08 -0400

    Adds encrypted credentials for Travis and updates project name

--- e2e_test.py
+++ e2e_test.py
@@ -11,7 +11,7 @@
 import urllib2
 import logging
 
-HOST='http://continuous-deployment-python.appspot.com'
+HOST='http://gasket-249003.appspot.com'
 
 # [START e2e]
 response = urllib2.urlopen("{}/get_author/ulysses".format(HOST))
